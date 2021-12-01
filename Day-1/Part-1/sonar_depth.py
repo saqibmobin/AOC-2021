358717b55a7429031b911ceb54e5c1db0ce0da1f
Saqib <saqibmobin@users.noreply.github.com>
2021-12-01 08:11:08 +0000

    day-1 part-1 solution

---
+++ Day-1/Part-1/sonar_depth.py
@@ -0,0 +1,26 @@
+input_filepath='./input/input.txt'
+sample_filepath='./input/sample.txt'
+
+def get_incremental_depth(depth):
+	count=0
+	prev=None
+	with open(depth, 'r') as f:
+		for line in f:
+			number=int(line.split()[0])
+			if prev==None:
+				prev=number
+			elif number>prev:
+				count+=1
+				prev=number
+			else:
+				prev=number
+	return count	
+	
+
+def main():
+	count = get_incremental_depth(input_filepath)
+	print(f"Number of times depth measurement increased is {count}")
+
+
+if __name__=='__main__':
+	main()
